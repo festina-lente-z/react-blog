d724176d9c99b5547f96d67776c404583768ac0c
BellaZZZZZ <bellazhao_321@qq.com>
2021-11-24 19:20:06 +0800

    update

--- src/container/Home/component/Banner/index.jsx
+++ src/container/Home/component/Banner/index.jsx
@@ -1,11 +1,10 @@
 import styles from './style.module.scss'
-import avatarImg from './avatar.jpeg'
 
 const Banner = () => {
   return (
     <div className={styles.banner}>
       <div className={styles.person}>
-        <img className={styles.avatar} src={avatarImg} alt="avatar"/>
+        <img className={styles.avatar} src="https://festinalente-serverless-project-static-files.oss-cn-beijing.aliyuncs.com/images/avatar.jpeg" alt="avatar"/>
         <div className={styles.title}>festinalente 的博客</div>
         <div className={styles.description}>热爱前端，热爱生活。</div>
       </div>
